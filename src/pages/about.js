e575a51fc423088f529b73cadd12e387d5b881d4
Nathan Phennel <nathanphennel@gmail.com>
2023-02-27 19:10:13 +1100

    feat: creates basic about page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! I'm Alan. This is the site. Welcome</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
