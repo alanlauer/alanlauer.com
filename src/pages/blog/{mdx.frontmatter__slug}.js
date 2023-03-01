8842e3799a0c3f3388d3b5ed083c55df4295662e
Nathan Phennel <nathanphennel@gmail.com>
2023-03-02 10:33:03 +1100

    feat: link blogs and pass (graphql) data and children (generated html) to blog pages

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,15 +1,28 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../../components/layout';
 import Seo from '../../components/seo';
 
-const BlogPost = () => {
+const BlogPost = ({ data, children }) => {
   return (
-    <Layout pageTitle="Super Cool Blog Posts">
-      <p>My blog post contents will go here (eventually).</p>
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      {children}
     </Layout>
   );
 };
 
-export const Head = () => <Seo title="Super Cool Blog Posts" />;
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />;
 
 export default BlogPost;
