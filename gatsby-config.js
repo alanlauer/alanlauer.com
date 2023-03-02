4b03a7976b4310bbe59313d2eff751bab01d3eb5
Nathan Phennel <nathanphennel@gmail.com>
2023-03-02 14:02:42 +1100

    feat: image support + blog post hero image logic

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,9 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     "gatsby-plugin-mdx",
     {
       resolve: "gatsby-source-filesystem",
