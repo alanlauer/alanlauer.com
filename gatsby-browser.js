6869df48854db9ab83dfb61f599f514460b8f5ab
Nathan Phennel <nathanphennel@gmail.com>
2023-05-29 20:23:28 +1000

    style: basic theme via global.css

---
+++ gatsby-browser.js
@@ -0,0 +1,15 @@
+
+// TODO: SET FONT // import "@fontsource/quicksand";
+import "./src/global.css";
+
+// not needed but keeping for reference, see:
+// https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/#wrapPageElement
+// Logs when the client route changes
+// exports.onRouteUpdate = ({ location, prevLocation }) => {
+
+// };
+
+// // Wraps every page in a component
+// exports.wrapPageElement = ({ element, props }) => {
+//   return <div {...props}>{element}</div>;
+// };
